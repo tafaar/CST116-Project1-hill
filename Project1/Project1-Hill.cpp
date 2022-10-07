b9375d6fe62a382a3b1524117fb015484b77e606
Aaron Hill <aaron.hill@oit.edu>
2022-10-07 15:10:08 -0700

    for loop input and output

--- Project1/Project1-Hill.cpp
+++ Project1/Project1-Hill.cpp
@@ -13,16 +13,28 @@ using std::string;
 class student {
 public:
 	string name;
-	int age;
-	float gpa;
+	int age = 0;
+	float gpa = 0;
 	
-	string printDetails() {
+	void printDetails() {
 		cout << name << " is " << age << " years old and has a GPA of " << gpa << endl;
 	};
 };
 
+student students[4];
+
 int main() {
 
+	for (int i = 0; i < 4; i++) {
+		cout << "Please enter the name, age, and GPA for student #" << i + 1 << endl;
+		cin >> students[i].name >> students[i].age >> students[i].gpa;
+	}
+
+	for (int i = 0; i < 4; i++) {
+		cout << students[i].name << " is " << students[i].age << " years old with a GPA of " << students[i].gpa << endl;
+	}
+
+	/*/
 	string name1, name2, name3, name4;
 	int age1, age2, age3, age4;
 	float gpa1, gpa2, gpa3, gpa4;
@@ -38,6 +50,8 @@ int main() {
 	cout << name2 << " is " << age2 << " years old, and their GPA is " << gpa2 << endl;
 	cout << name3 << " is " << age3 << " years old, and their GPA is " << gpa3 << endl;
 	cout << name4 << " is " << age4 << " years old, and their GPA is " << gpa4 << endl;
+
+	/*/
 
 	return 0;
 }
