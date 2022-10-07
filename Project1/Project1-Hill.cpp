95df08d2ff1fb74430da33ed6d8c201b339e5446
Aaron Hill <aaron.hill@oit.edu>
2022-10-07 14:13:09 -0700

    version 1

--- Project1/Project1-Hill.cpp
+++ Project1/Project1-Hill.cpp
@@ -1,3 +1,7 @@
+/*
+* CST 116, Aaron Hill
+*/
+
 #include <iostream>
 #include <string>
 
@@ -8,24 +12,21 @@ using std::string;
 
 int main() {
 
-	string name1;
-	int age1;
-	float gpa1;
+	string name1, name2, name3, name4;
+	int age1, age2, age3, age4;
+	float gpa1, gpa2, gpa3, gpa4;
 
-	cout << "Hello! Please enter a name." << endl;
+	cout << "Hello! Please enter 4 names, each separated by a space." << endl;
+	cin >> name1 >> name2 >> name3 >> name4;
+	cout << "Now enter the age of each of those people, again separated by a space" << endl;
+	cin >> age1 >> age2 >> age3 >> age4;
+	cout << "Please enter each person's GPA, also separated by a space." << endl;
+	cin >> gpa1 >> gpa2 >> gpa3 >> gpa4;
 
-	cin >> name1;
-
-	cout << "Now enter your age." << endl;
-
-	cin >> age1;
-
-	cout << "What's your GPA?" << endl;
-
-	cin >> gpa1;
-
-	cout << "Your name is " << name1 << ", you are " << age1 << " years old, and your GPA is " << gpa1 << endl;
-
+	cout << name1 << " is " << age1 << " years old, and their GPA is " << gpa1 << endl;
+	cout << name2 << " is " << age2 << " years old, and their GPA is " << gpa2 << endl;
+	cout << name3 << " is " << age3 << " years old, and their GPA is " << gpa3 << endl;
+	cout << name4 << " is " << age4 << " years old, and their GPA is " << gpa4 << endl;
 
 	return 0;
 }
